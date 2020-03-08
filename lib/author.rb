f85e3c7aca26e9f6e833e2208894b6ff8f5dff10
Terry Threatt <terry.threatt@gmail.com>
2020-03-08 23:04:57 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -2,6 +2,7 @@ class Author
   attr_accessor :posts, :name  
   
   @@post_count = 0
+  
   def initialize(name) 
     @name = name
     @posts = []
@@ -10,4 +11,8 @@ class Author
   def posts
    Post.all.select {|post| post.author = self}
   end 
+  
+  def self.post_count
+    @@post_count
+  end
 end 

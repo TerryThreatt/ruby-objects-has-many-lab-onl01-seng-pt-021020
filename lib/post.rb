c8314923954cc1bd7ca9d05626ff1d51ce5cb05e
Terry Threatt <terry.threatt@gmail.com>
2020-03-08 22:51:17 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,3 +1,10 @@
 class Post 
-  attr_accessor :author, :posts  
+  attr_accessor :author, :posts, :title 
+  
+  @@all = []
+  def initialize(title)
+    @title = title
+    @all << self 
+  end 
+  
 end 

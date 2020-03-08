19ca9cd92eabde7318f18f2b7ca1abeb43dd4912
Terry Threatt <terry.threatt@gmail.com>
2020-03-08 21:59:03 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -14,14 +14,14 @@ class Artist
   
   def add_song(song)
     @songs << song 
-    self.artist = self
+    self.artist = song 
     @@song_count +=1
   end 
   
   def add_song_by_name(name)
     song = Song.new(name) 
     @songs << song 
-    self.artist = self 
+    self.artist = song  
     @@song_count += 1 
   end 
   

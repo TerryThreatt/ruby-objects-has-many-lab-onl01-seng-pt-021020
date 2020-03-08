7eba8688ca011929b2c9890fca8615ad10baa9e6
Terry Threatt <terry.threatt@gmail.com>
2020-03-08 23:07:10 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -16,6 +16,11 @@ class Author
     post.author = self 
   end 
   
+  def add_post_by_title(title)
+    post = Post.new(title)
+    add_post(post)
+  end 
+  
   def self.post_count
     @@post_count
   end
